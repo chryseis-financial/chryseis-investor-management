f44b4bfd9db0ac835a8b05b9f491f912ebac390d
Balaji Varadharajan <vbalaji215@gmail.com>
2020-05-20 13:50:56 -0400

    Updated Controller

--- src/main/java/com/chryseis/investor/management/web/controllers/InvestorManagementController.java
+++ src/main/java/com/chryseis/investor/management/web/controllers/InvestorManagementController.java
@@ -1,15 +1,8 @@
 package com.chryseis.investor.management.web.controllers;
 
 import com.chryseis.investor.management.web.api.InvestorManagementApi;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
